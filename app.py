5cb2773fc403ac578a32ef1eb591f2e5145853e9
lucianosalto19-star <lucianosalto19@gmail.com>
2026-03-10 18:37:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.write("# Tablero EcoBici")
-st.caption("Visualizaci162n y Storytelling Usando Datos")
+st.caption("Visualización y Storytelling Usando Datos")
 st.caption("Luciano Salto")
